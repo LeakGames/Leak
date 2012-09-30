86777d70e109ae890bb5dee4463afd055347aeb8
Nyu <admin@queeply.com>
2012-10-01 00:15:49 +0300

    Update src/core/grid.h

--- src/core/grid.h
+++ src/core/grid.h
@@ -1,12 +1,28 @@
 #include <vector>
 
-class Grid {
-public:
-  Grid();
-  ~Grid();
-  bool is_cell_free(unsigned int x, unsigned int y);
-  void spawn_bonuses();
+using namespace std;
 
+class Grid
+{
 protected:
-  Vector<short int> *vector;
-}
+    int n, m;
+
+public:
+    vector<vector<int> > matrix;
+
+    Grid( const int n, const int m ) {
+      vector<vector<int> > matrix( n, vector<int>( m ) );
+
+      this->n = n;
+      this->m = m;
+      this->matrix = matrix;
+    }
+
+    int operator()(int x, int y) {
+        return this->matrix[x][y];
+    }
+    
+    ~Grid();
+    bool is_cell_free(unsigned int x, unsigned int y);
+    void spawn_bonuses();
+};
