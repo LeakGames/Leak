fcc95b6e058545064adb2be7a59bab3159870d0d
0mg lool <nyutsuki@gmail.com>
2012-09-30 23:22:58 +0200

    UPDATE && PUSHED grid.cpp

---
+++ src/core/grid.cpp
@@ -0,0 +1,18 @@
+﻿#include <vector>
+#include "main.h"
+
+using namespace std;
+
+Grid::Grid( const int n, const int m ) {
+  vector<vector<int> > matrix( n, vector<int>( m ) );
+
+  this->n = n;
+  this->m = m;
+  this->matrix = matrix;
+}
+
+int Grid::operator()(int x, int y) {
+    return this->matrix[x][y];
+}
+
+int main() {}
