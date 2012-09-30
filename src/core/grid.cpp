924b13d6f4b934d795433559ef081f5d207893b6
Nyu <admin@queeply.com>
2012-10-01 00:52:42 +0300

    Update src/core/grid.cpp

--- src/core/grid.cpp
+++ src/core/grid.cpp
@@ -1,5 +1,8 @@
 #include <vector>
-#include "grid.h"
+#include <cstdlib>
+#include <ctime>
+
+#include "main.h"
 
 using namespace std;
 
@@ -9,6 +12,9 @@ Grid::Grid( const int n, const int m ) {
   this->n = n;
   this->m = m;
   this->matrix = matrix;
+
+  for( int i = 1; i <= 6; i++ )
+      this->gift.push_back( i * -1 );
 }
 
 int Grid::operator()(int x, int y) {
@@ -16,7 +22,24 @@ int Grid::operator()(int x, int y) {
 }
 
 bool Grid::is_cell_free( const int x, const int y ) {
-    return (bool) this->matrix[x][y];
+    return !( ( bool ) this->matrix[x][y] );
+}
+
+void Grid::spawn_bonuses() {
+    srand(time(0));
+
+    int rand_n, rand_m, rand_gift;
+
+    do {
+        rand_n = rand() % this->n;
+        rand_m = rand() % this->m;
+        rand_gift = rand() % 6;
+
+        if( this->is_cell_free( rand_n, rand_m ) ) {
+            this->matrix[ rand_n ][ rand_m ] = this->gift.at( rand_gift );
+        }
+
+    } while( this->is_cell_free( rand_n, rand_m ) );
 }
 
 int main() {}
