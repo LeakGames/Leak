899ce4414edf133e4f7e8a17b61deeb77ec77c26
stoke <stoke95@yahoo.it>
2012-10-01 16:42:52 +0300

    Update src/core/player.h

--- src/core/player.h
+++ src/core/player.h
@@ -1,15 +1,15 @@
 #include <SFML/Graphics.h>
+#include <vector>
 
-class Player 
-{
+class Player {
 public:
-    sf::Color color;
+    sf::RectangleShape rect;
     int starting_x, starting_y, kills, deaths;
     vector<int> *bonuses;
     float exp;
 
     Player();
     ~Player();
-    int move( int x, int y );
-    int scan( int x, int y );
+    int move(const int sx, const int sy, const int x, const int y);
+    int scan(const int x, const int y);
 };
