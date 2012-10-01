0b06f58d518192f050d0b54848447b135da4ee17
Emanuele Sgroi <emanuele.sgroi@gmail.com>
2012-10-01 15:51:07 +0200

    Update gui

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -1,12 +1,43 @@
 #include "gui.h"
 
-using namespace std;
-
-Gui::Gui(const int w = 100, const int h = 100) {
-	vector<vector<sf::RectangleShape> > matrix( w, vector<sf::RectangleShape>( h ));
-
-	this->window.create(sf::VideoMode(w, h), "Leak");
+Gui::Gui(const int w = 500, const int h = 500) {
+	this->w = w;
+	this->h = h;
+	vector<vector<sf::RectangleShape> > matrix(w, vector<sf::RectangleShape>( h ));
 	this->matrix = matrix;
+	this->window.create(sf::VideoMode(w, h), "Leak");
+}
+
+void Gui::create_matrix(int w, int h) {
+	int x, y;
+	
+	for (x = 0; x < this->w/w; x++) {
+		for (y = 0; y < this->h/h; y++) {
+			this->matrix[x][y].setSize(sf::Vector2f(50, 50));
+			this->matrix[x][y].setOutlineColor(sf::Color::Red);
+			this->matrix[x][y].setOutlineThickness(2);
+			this->matrix[x][y].setPosition(x*50, y*50);
+		}
+	}
+}
+
+void Gui::display_window(int w, int h) { 
+	int x, y;
+	
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+        for (x = 0; x < this->w/w; x++) {
+			for (y = 0; y < this->h/h; y++) {
+				this->window.draw(this->matrix[x][y]);
+			}
+		}
+		this->window.display();
+	}
 }
 
 int main() {
@@ -44,8 +75,12 @@ int main() {
 		window.draw(text);
      	window.display();
 	}*/
-
+	int w, h;
+	w = 50;
+	h = 50;
 	Gui *g = new Gui();
+	g->create_matrix(w, h);
+	g->display_window(w, h);
 
 	return 0;
 }
--- src/gui/gui.h
+++ src/gui/gui.h
@@ -5,12 +5,16 @@ using namespace std;
 
 class Gui {	
 	public:
+		int w, h;
 		Gui(const int, const int);
 		~Gui();
+		void create_matrix(int, int);
+		void display_window(int, int);
 
 	protected:
 		sf::RenderWindow window;
 		sf::Font font;
 		sf::Text text;
 		vector< vector<sf::RectangleShape> > matrix;
+		sf::RectangleShape rectangle;
 };
