4c1aa797a0ccafc94870e980d8941f63180dee73
Nyu <admin@queeply.com>
2012-10-01 16:07:15 +0300

    Update src/core/grid.h

--- src/core/grid.h
+++ src/core/grid.h
@@ -15,5 +15,7 @@ public:
     int operator()( const int x, const int y);
     bool is_cell_free( const int x, const int y );
     void spawn_bonuses();
-    void set(int x, int y, int n);
+    void set(int x, int y, int n);    
+    void spawn_player( const int cell_free );
+    void print_matrix();
 };
